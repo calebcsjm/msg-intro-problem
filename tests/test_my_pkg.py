c74203b7ab8f870bf11a2ddb4feb5362b4efa22d
calebcsjm <65317377+calebcsjm@users.noreply.github.com>
2020-11-20 12:47:35 -0700

    Update test_my_pkg.py

--- tests/test_my_pkg.py
+++ tests/test_my_pkg.py
@@ -1,1 +1,14 @@
+""" Tests the mathematical functions defined in my_pkg/trial.py
+"""
 
+import pytest
+
+def test_square():
+  """ Tests the squaring function"""
+  
+  from my_pkg.trial import square
+  assert 4 == square(2)
+  assert 4 == square(-2)
+  assert 12.25 == square(3.5)
+  assert 2 == round(square(sqrt(2)), 5)
+  
