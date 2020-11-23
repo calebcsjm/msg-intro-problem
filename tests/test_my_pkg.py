36c17404605e69050fb5cbe67531d72d8b8e013d
calebcsjm <65317377+calebcsjm@users.noreply.github.com>
2020-11-23 15:17:39 -0700

    Update test_my_pkg.py

--- tests/test_my_pkg.py
+++ tests/test_my_pkg.py
@@ -12,3 +12,15 @@ def test_square():
   assert 12.25 == square(3.5)
   assert 2 == round(square(sqrt(2)), 5)
   
+def test_factorial():
+  """Tests the factorial function."""
+  
+  from my_pkg.trial import factorial
+  
+  assert 24 == factorial(4)
+  assert 6 == factorial(3.0)
+  assert 1 == factorial(0)
+  assert 1 == factorial(-1)
+  
+  with pytest.raises(ValueError): 
+    factorial(3.5)
